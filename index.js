0151771ddd51137c15ea626e11855eeded59875e
Tee Jae Calinao <teejae.calinao@tuitt.com>
2023-10-11 14:36:43 +0800

    updated

---
+++ index.js
@@ -0,0 +1,13 @@
+let navbar = document.getElementById("navbar");
+
+document.addEventListener("scroll",()=>{
+
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+
+
+
+})
